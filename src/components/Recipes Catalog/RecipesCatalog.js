316ee82db557dcc1b2a1c8000a89568cd00e66a8
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-03-21 11:04:47 +0200

    responsive recipes catalog page

--- src/components/Recipes Catalog/RecipesCatalog.js
+++ src/components/Recipes Catalog/RecipesCatalog.js
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from "./styles/recipesCatalog.module.css";
+import resp from "./styles/responsive.module.css";
 
 const RecipesCatalog = () => {
     return (
-        <div className={styles["main-wrapper"]}>
-            <Link className={styles["catalog-recipes"]} to="/catalog-recipes" replace>
-                Catalog Recipes
-            </Link>
-            <Link className={styles["catalog-products"]} to="/catalog-products" replace>
-                Catalog Products
-            </Link>
-            <section className={styles["catalog"]}>
+        <div className={`${styles["main-wrapper"]} ${resp["main-wrapper"]}`}>
+            <div className={`${styles["links-wrapper"]} ${resp["links-wrapper"]}`}>
+                <Link className={`${styles["catalog-recipes"]} ${resp["catalog-recipes"]}`} to="/catalog-recipes" replace>
+                    Catalog Recipes
+                </Link>
+                <Link className={`${styles["catalog-products"]} ${resp["catalog-products"]}`} to="/catalog-products" replace>
+                    Catalog Products
+                </Link>
+            </div>
+            <section className={`${styles["catalog"]} ${resp["catalog"]}`}>
                 <article className={styles["catalog-recipe"]}>
                     <img
                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLpyUBeuso0JIz0uvwEUW8ieyBzNsnSe4GiA&usqp=CAU"
