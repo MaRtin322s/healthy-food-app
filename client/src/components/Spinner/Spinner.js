8ba3482154691e4b9e65e839714a0bdf8e5c1ef2
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-03-22 17:48:40 +0200

    add spinner component

---
+++ client/src/components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import styles from "./styles/spinner.module.css";
+
+const Spinner = () => {
+    return (
+        <div className={styles["loader"]}>Loading...</div>
+    );
+}
+
+export default Spinner;
