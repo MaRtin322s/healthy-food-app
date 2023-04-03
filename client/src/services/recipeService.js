76baf7d399c10624d104cf2ca785941892c844d5
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-04-03 14:20:49 +0300

    async/await
    implement async/await instead of then/catch for recipe services

--- client/src/services/recipeService.js
+++ client/src/services/recipeService.js
@@ -1,65 +1,65 @@
 const baseUrl = "http://localhost:3030/recipes";
 
-export const createRecipe = (token, data) => {
-    return fetch(`${baseUrl}/create`, {
+export const createRecipe = async (token, data) => {
+    const res = await fetch(`${baseUrl}/create`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
             'X-Authorization': token
         },
         body: JSON.stringify(data)
-    })
-        .then(res => res.json());
+    });
+    return await res.json();
 };
 
 export const getAll = () => fetch(`${baseUrl}/all`).then(res => res.json());
 export const getOne = (recipeId) => fetch(`${baseUrl}/${recipeId}`).then(res => res.json());
 export const getOwned = (userId) => fetch(`${baseUrl}/profile/${userId}`).then(res => res.json());
 
-export const deleteRecipe = (id, token) => {
-    return fetch(`${baseUrl}/delete/${id}`, {
+export const deleteRecipe = async (id, token) => {
+    const res = await fetch(`${baseUrl}/delete/${id}`, {
         method: "DELETE",
         headers: {
             'X-Authorization': token
         }
-    })
-        .then(res => res.json());
+    });
+    return await res.json();
 };
 
-export const editRecipe = (recipeData, token, id) => {
-    return fetch(`${baseUrl}/edit/${id}`, {
+export const editRecipe = async (recipeData, token, id) => {
+    const res = await fetch(`${baseUrl}/edit/${id}`, {
         method: "PUT",
         headers: {
             'Content-Type': 'application/json',
             'X-Authorization': token
         },
         body: JSON.stringify(recipeData)
-    })
-        .then(res => res.json());
+    });
+    return await res.json();
 };
 
-export const saveRecipe = (recipeId, userId, token) => {
-    return fetch(`${baseUrl}/save/${recipeId}`, {
+export const saveRecipe = async (recipeId, userId, token) => {
+    const res = await fetch(`${baseUrl}/save/${recipeId}`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
             'X-Authorization': token
         },
         body: JSON.stringify({ userId })
-    })
-        .then(res => res.json());
+    });
+    return await res.json();
 };
 
 export const getSavedRecipes = (userId) => fetch(`${baseUrl}/save/${userId}`).then(res => res.json());
 
-export const download = (recipeData, token) => {
-    return fetch(`${baseUrl}/download`, {
+export const download = async (recipeData, token) => {
+    const res = await fetch(`${baseUrl}/download`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
             'X-Authorization': token
         },
         body: JSON.stringify(recipeData)
-    })
-        .then(res => res.blob());
-}
+    });
+    return await res.json();    
+};
