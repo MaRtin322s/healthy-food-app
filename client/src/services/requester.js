5b5bf8d02758416d0cfb5a5a63569058da38e0ab
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-08-20 21:13:59 +0300

    implement requester post method

--- client/src/services/requester.js
+++ client/src/services/requester.js
@@ -1,4 +1,4 @@
-export const requester = {
+const requester = {
     get: async (url, headers = {}) => {
         try {
             const res = await fetch(url, {
@@ -17,5 +17,29 @@ export const requester = {
         } catch (err) {
             return err;
         }
+    },
+
+    post: async (url, data = {}, headers = {}) => {
+        try {
+            const res = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    ...headers
+                },
+                body: JSON.stringify(data)
+            });
+
+            if (!res.ok) {
+                throw new Error(`Request failed with status: ${res.status}`);
+            } else {
+                return res.json();
+            }
+
+        } catch (err) {
+            return err;
+        }
     }
 };
+
+export default requester;
