f5f422b4d9b6125852cc95866b49d23350ca6f97
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-09-03 00:01:00 +0300

    test case for login page title

---
+++ client/src/components/Login/tests/Login.test.js
@@ -0,0 +1,30 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Login from '../Login';
+import { AuthContext } from '../../../contexts/UserContext';
+import { BrowserRouter } from 'react-router-dom';
+
+const renderLoginComponent = () => {
+    const mockContext = {
+        userLogin: () => null,
+    };
+    render(
+        <BrowserRouter>
+            <Login />
+        </BrowserRouter>,
+        {
+            wrapper: ({ children }) =>
+                <AuthContext.Provider value={mockContext}>
+                    {children}
+                </AuthContext.Provider>
+        }
+    );
+};
+
+describe("Home component functionality tests", () => {
+    test("Renders the login page main title", () => {
+        renderLoginComponent();
+        const heading = screen.getByText(/Login for users/i);
+        expect(heading).toBeInTheDocument();
+    });
+});
