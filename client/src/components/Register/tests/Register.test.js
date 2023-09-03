dd0bc0ca8f893e84d172aff8af84f576dc521af0
Martin Sofroniev <marti.sofroniev0408@gmail.com>
2023-09-03 19:34:02 +0300

    render register page test

---
+++ client/src/components/Register/tests/Register.test.js
@@ -0,0 +1,32 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import Register from '../Register';
+import { AuthContext } from '../../../contexts/UserContext';
+import { BrowserRouter } from 'react-router-dom';
+
+const mockContext = {
+    userLogin: () => null,
+    mockLogin: jest.fn()
+};
+
+const renderRegisterComponent = () => {
+    render(
+        <BrowserRouter>
+            <Register />
+        </BrowserRouter>,
+        {
+            wrapper: ({ children }) =>
+                <AuthContext.Provider value={mockContext}>
+                    {children}
+                </AuthContext.Provider>
+        }
+    );
+};
+
+describe('Register component tests', () => {
+    test('Render register page', () => {
+        renderRegisterComponent();
+        const heading = screen.getByText('Register new users');
+        expect(heading).toBeInTheDocument();
+    });
+});
