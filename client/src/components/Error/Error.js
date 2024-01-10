f03ee423c45ee136d27744b0ce25cf803d12ea4a
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-01-11 01:19:01 +0200

    responsive error notification

--- client/src/components/Error/Error.js
+++ client/src/components/Error/Error.js
@@ -1,9 +1,10 @@
 import styles from "./styles/error.module.css";
+import resp from './styles/error.responsive.module.css';
 
 const Error = ({ message }) => {
     return (
-        <div id={styles["notification-modal"]}>
-            <div className={styles["modal-content"]}>
+        <div id={`${styles["notification-modal"]} ${resp["notification-modal"]}`}>
+            <div className={`${styles["modal-content"]} ${resp["modal-content"]}`}>
                 <h2>Notification</h2>
                 <p>{message}</p>
             </div>
