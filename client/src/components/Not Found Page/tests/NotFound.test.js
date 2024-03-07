ce675df30ee1d38e00d064d823d520e9cd733a9e
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-03-08 01:26:56 +0200

    test not found page component

---
+++ client/src/components/Not Found Page/tests/NotFound.test.js
@@ -0,0 +1,21 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import NotFound from '../NotFound';
+import { AuthContext } from '../../../contexts/UserContext';
+import { BrowserRouter } from 'react-router-dom';
+
+const renderNotFoundComponent = () => {
+    const mockContext = {};
+    render(
+        <BrowserRouter>
+            <NotFound />
+        </BrowserRouter>,
+        {
+            wrapper: ({ children }) =>
+                <AuthContext.Provider
+                    value={mockContext}>
+                    {children}
+                </AuthContext.Provider>
+        }
+    );
+};
