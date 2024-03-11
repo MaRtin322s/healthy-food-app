f1ecd293c4fcd7693ec6e43fe01442ce93bfc7e5
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-03-11 17:23:18 +0200

    redesing bmi design

--- client/src/components/BMI Calculator/BMICalculator.js
+++ client/src/components/BMI Calculator/BMICalculator.js
@@ -46,68 +46,70 @@ export const BMICalculator = () => {
     };
 
     return (
-        <section className={`${styles['bmi-section']}`}>
-            <h3 className={`${styles['bmi']}`}>
-                <b>B</b>ody <b>M</b>ass <b>I</b>ndex Calculator
-            </h3>
-            <form className={`${styles['bmi-form']} ${resp['bmi-form-resp']}`} id="form" onSubmit={(ev) => submitHandler(ev, data)}>
-                <div className={`${styles['row-one']}`}>
-                    <input
-                        type="text"
-                        className={`${styles['text-input']}`}
-                        name="age"
-                        id="age"
-                        autoComplete="off"
-                        required={true}
-                        value={data.age}
-                        onChange={(e) => changeHandler(e)}
-                    />
-                    <p className={`${styles['text']}`}>Age</p>
-                    <label className={`${styles['container']}`}>
-                        <input type="radio" name="radio" id="f" value="f" onChange={(e) => changeHandler(e)} />
-                        <p className={`${styles['text']}`}>Female</p>
-                        <span className={`${styles['checkmark']}`} />
-                    </label>
-                    <label className={`${styles['container']}`}>
-                        <input type="radio" name="radio" id="m" value="m" onChange={(e) => changeHandler(e)} />
-                        <p className={`${styles['text']}`}>Male</p>
-                        <span className={`${styles['checkmark']}`} />
-                    </label>
-                </div>
-                <div className="row-two">
-                    <input
-                        type="text"
-                        className={`${styles['text-input']}`}
-                        id="height"
-                        name='height'
-                        autoComplete="off"
-                        required=""
-                        value={data.height}
-                        onChange={(e) => changeHandler(e)}
-                    />
-                    <p className={`${styles['text']}`}>Height (cm)</p>
-                    <input
-                        type="text"
-                        className={`${styles['text-input']}`}
-                        id="weight"
-                        name='weight'
-                        autoComplete="off"
-                        required=""
-                        value={data.weight}
-                        onChange={(e) => changeHandler(e)}
-                    />
-                    <p className={`${styles['text']}`}>Weight (kg)</p>
-                </div>
-                <button
-                    className={`${styles['submit']}`}
-                    id="submit"
-                >
-                    Submit
-                </button>
-            </form>
+        <>
+            <section className={`${styles['bmi-section']}`}>
+                <h3 className={`${styles['bmi']}`}>
+                    <b>B</b>ody <b>M</b>ass <b>I</b>ndex Calculator
+                </h3>
+                <form className={`${styles['bmi-form']} ${resp['bmi-form-resp']}`} id="form" onSubmit={(ev) => submitHandler(ev, data)}>
+                    <div className={`${styles['row-one']}`}>
+                        <input
+                            type="text"
+                            className={`${styles['text-input']}`}
+                            name="age"
+                            id="age"
+                            autoComplete="off"
+                            required={true}
+                            value={data.age}
+                            onChange={(e) => changeHandler(e)}
+                        />
+                        <p className={`${styles['text']}`}>Age</p>
+                        <label className={`${styles['container']}`}>
+                            <input type="radio" name="radio" id="f" value="f" onChange={(e) => changeHandler(e)} />
+                            <p className={`${styles['text']}`}>Female</p>
+                            <span className={`${styles['checkmark']}`} />
+                        </label>
+                        <label className={`${styles['container']}`}>
+                            <input type="radio" name="radio" id="m" value="m" onChange={(e) => changeHandler(e)} />
+                            <p className={`${styles['text']}`}>Male</p>
+                            <span className={`${styles['checkmark']}`} />
+                        </label>
+                    </div>
+                    <div className="row-two">
+                        <input
+                            type="text"
+                            className={`${styles['text-input']}`}
+                            id="height"
+                            name='height'
+                            autoComplete="off"
+                            required=""
+                            value={data.height}
+                            onChange={(e) => changeHandler(e)}
+                        />
+                        <p className={`${styles['text']}`}>Height (cm)</p>
+                        <input
+                            type="text"
+                            className={`${styles['text-input']}`}
+                            id="weight"
+                            name='weight'
+                            autoComplete="off"
+                            required=""
+                            value={data.weight}
+                            onChange={(e) => changeHandler(e)}
+                        />
+                        <p className={`${styles['text']}`}>Weight (kg)</p>
+                    </div>
+                    <button
+                        className={`${styles['submit']}`}
+                        id="submit"
+                    >
+                        Submit
+                    </button>
+                </form>
 
-            <h1 className={`${styles['bmi']}`}>Result: {data.result ? data.result : 'TBD'}</h1>
-            <h2 className={`${styles['bmi']}`}>Index: {data.bmi ? data.bmi : 'TBD'}</h2>
-        </section>
+                <h1 className={`${styles['bmi']}`}>Result: {data.result ? data.result : 'TBD'}</h1>
+                <h2 className={`${styles['bmi']}`}>Index: {data.bmi ? data.bmi : 'TBD'}</h2>
+            </section>
+        </>
     );
 };
