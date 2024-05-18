96d718ba4e285a8ecb1755f8cf160c5e35ae466b
Martin Sofroniev <m.sofroniev12@gmail.com>
2024-05-19 01:21:58 +0300

    daily meal plan style changes

--- client/src/components/BMI Calculator/DailyMealElement.js
+++ client/src/components/BMI Calculator/DailyMealElement.js
@@ -7,8 +7,8 @@ export function DailyMealElement(props) {
                 <h5 className={styles['bmi']}>{props.day}</h5>
                 <div className={styles['daily-meals']}>
                     <div className="card" style={{ width: "15rem", backgroundColor: "#19e79c" }}>
-                        <div className="card-body">
-                            <h5  style={{ width: "150px", height: "80px", fontSize: "21px" }}>{props.title}</h5>
+                        <div className={styles["card-body"]}>
+                            <h5 style={{ height: "80px", fontSize: "21px", padding: '40px' }}>{props.title}</h5>
                         </div>
                         <ul className="list-group list-group-flush">
                             <li className="list-group-item">
